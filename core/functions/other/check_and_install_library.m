a9fb4aedc75646856f64589efb7c6646b5160205
ispamm <simone.scardapane@uniroma1.it>
2014-04-08 10:08:53 +0200

    Small change in installation script
    The change is used to warn the user of the functionality depending on a
    library, before the user chooses whether or not to install it.

--- core/functions/other/check_and_install_library.m
+++ core/functions/other/check_and_install_library.m
@@ -2,10 +2,11 @@
 % CHECK_AND_INSTALL_LIBRARY Verify if a library is present, and eventally
 % install it.
 %
-%   CHECK_AND_INSTALL_LIBRARY(NAME, FUNC, URL, STRINGONERROR) Searches if
+%   CHECK_AND_INSTALL_LIBRARY(NAME, FUNC, URL, REQUIRED) Searches if
 %   library NAME is present by verifying that the function FUNC is
 %   available on the path. Is it is not present, downloads the library from
-%   URL, otherwise warn the user with the string STRINGONERROR.
+%   URL, otherwise warn the user. REQUIRED is a string describing the
+%   functionality (or functionalities) that depends on the library.
 
 % License to use and modify this code is granted freely without warranty to all, as long as the original author is
 % referenced and attributed as such. The original author maintains the right to be solely associated with this work.
@@ -13,15 +14,15 @@
 % Programmed and Copyright by Simone Scardapane:
 % simone.scardapane@uniroma1.it
 
-function check_and_install_library( lib_name, lib_function, lib_url, stringOnError )
+function check_and_install_library( lib_name, lib_function, lib_url, stringRequired )
 
 fprintf('Checking for presence of %s...\n', lib_name);
 res = exist(lib_function, 'file');
 if(res ~= 2 && res ~= 3)
     result = '';
     while(~strcmpi(result, 'Y') && ~strcmpi(result, 'N'))
-        inputString = sprintf('%s not found. Do you want to install it? (Y/N) ', lib_name);
-        result = input(inputString, 's');
+        cprintf('*text', '--- %s not found. Do you want to install it? (Y/N) ---\n(Required for: %s)\n', lib_name, stringRequired);
+        result = input('', 's');
     end
     if(strcmpi(result, 'Y'))
         fprintf('Downloading toolbox... (may take some minutes)\n');
@@ -35,7 +36,7 @@ if(res ~= 2 && res ~= 3)
            error('LearnToolbox:FatalError:ToolboxDownloadError', 'There was an error in downloading the toolbox'); 
         end
     else
-        warning('LearnToolbox:Warning:ToolboxNotInstalled', stringOnError);
+        warning('LearnToolbox:Warning:ToolboxNotInstalled', 'You will not be able to use the following: %s', stringRequired);
     end
 end
 
